07d7e0aeeb68de760f50fe2d59ea9d52f75c7b5f
Martyn Janes <martyn@obany.com>
2017-12-18 10:27:35 +0300

    Added JavaScript SystemJS Builds

--- create-apps.sh
+++ create-apps.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 set -e
 
-node unitejs/unitejs-cli/bin/unite configure --packageName=angular-javascript --title="Angular JavaScript" --profile=AngularJavaScript --outputDirectory=./apps/angular-javascript
-mkdir -p ./apps/angular-javascript/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
-mkdir -p ./apps/angular-javascript/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
-node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/angular-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/angular-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/angular-javascript
-node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/angular-javascript
+node unitejs/unitejs-cli/bin/unite configure --packageName=angular-javascript-commonjs --title="Angular JavaScript CommonJS" --profile=AngularJavaScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/angular-javascript-commonjs
+mkdir -p ./apps/angular-javascript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/angular-javascript-commonjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/angular-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/angular-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/angular-javascript-commonjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/angular-javascript-commonjs
+
+node unitejs/unitejs-cli/bin/unite configure --packageName=angular-javascript-systemjs --title="Angular JavaScript SystemJS" --profile=AngularJavaScript --moduleType=SystemJS --bundler=SystemJSBuilder --outputDirectory=./apps/angular-javascript-systemjs
+mkdir -p ./apps/angular-javascript-systemjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.js $_
+mkdir -p ./apps/angular-javascript-systemjs/www/test/e2e/src/helpers/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/helpers/*.js $_
+node unitejs/unitejs-cli/bin/unite package --packageName=lodash --outputDirectory=./apps/angular-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=moment --outputDirectory=./apps/angular-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=rxjs --outputDirectory=./apps/angular-javascript-systemjs
+node unitejs/unitejs-cli/bin/unite package --packageName=underscore --outputDirectory=./apps/angular-javascript-systemjs
 
 node unitejs/unitejs-cli/bin/unite configure --packageName=angular-typescript-commonjs --title="Angular TypeScript CommonJS" --profile=AngularTypeScript --moduleType=CommonJS --bundler=Webpack --outputDirectory=./apps/angular-typescript-commonjs
 mkdir -p ./apps/angular-typescript-commonjs/www/test/e2e/src/examples/ && cp -r ./unitejs/unitejs-packages/test/e2e/src/examples/*.ts $_
